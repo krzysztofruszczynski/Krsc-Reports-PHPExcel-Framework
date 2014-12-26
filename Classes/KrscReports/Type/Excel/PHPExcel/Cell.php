375a92599340e2975b33cfd2d82b1b70af85426f
ruszczak <ruszczak@o2.pl>
2014-12-26 12:40:06 +0100

    phpdoc documentation added for type section

--- Classes/KrscReports/Type/Excel/PHPExcel/Cell.php
+++ Classes/KrscReports/Type/Excel/PHPExcel/Cell.php
@@ -1,36 +1,42 @@
 <?php
 /**
-  @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
+ * Class which one object represents one cell. It applies styles, value and type of cell.
+ * At the end cell with given coordinates and properties can be constructed (methods inside class
+ * use directly PHPExcel commands). 
+ * 
+ * @category KrscReports
+ * @package KrscReports_Type
+ * @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
  */
 class KrscReports_Type_Excel_PHPExcel_Cell
 {
     /**
-     * @var PHPExcel 
+     * @var PHPExcel instance of used PHPExcel object
      */
     protected static $_oPHPExcel;
     
     /**
-     * @var Mixed
+     * @var mixed value of current cell
      */
     protected $_mValue;
     
     /**
-     * @var String 
+     * @var string type of current cell 
      */
     protected $_sType;
     
     /**
-     * @var KrscReports_Type_Excel_PHPExcel_Style 
+     * @var KrscReports_Type_Excel_PHPExcel_Style object for managing style collections
      */
     protected $_oStyle;
     
     /**
-     * @var String
+     * @var string currently selected key of style collection
      */
     protected $_sStyleKey = KrscReports_Type_Excel_PHPExcel_Style::KEY_DEFAULT;
     
     /**
-     * Update of PHPExcel object.
+     * Constructor which updates PHPExcel instance inside a class.
      * @return Void
      */
     public function __construct()
@@ -38,31 +44,45 @@ class KrscReports_Type_Excel_PHPExcel_Cell
         self::$_oPHPExcel = KrscReports_Builder_Excel_PHPExcel::getPHPExcelObject();
     }
     
+    /**
+     * Method for setting object for managing style collections.
+     * @param KrscReports_Type_Excel_PHPExcel_Style $oStyle instance of object for managing style collections
+     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
+     */
     public function setStyleObject( KrscReports_Type_Excel_PHPExcel_Style $oStyle )
     {
         $this->_oStyle = $oStyle;
-    }
-    
-    public function setValue( $mValue )
-    {
-        $this->_mValue = $mValue;
-    }
-    
-    public function setFormula( $mFormula )
-    {
-        
-    }
-    
-    public function setType( $sType )
-    {
-        $this->_sType = $sType;
+        return $this;
     }
     
     /**
-     * 
-     * @param String $sStyleKey
-     * @param Boolean $bIfStyleNotExistsSelectDefault
-     * @return Boolean true if style was found, false otherwise
+     * Method for setting value of current cell.
+     * @param mixed $mValue value of current cell (can be text, numeric or formula: starts with =)
+     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
+     */
+    public function setValue( $mValue )
+    {
+        $this->_mValue = $mValue;
+        return $this;
+    }
+    
+    /**
+     * Method setting type of cell.
+     * @param string $sType type of cell to be set
+     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
+     */
+    public function setType( $sType )
+    {
+        $this->_sType = $sType;
+        return $this;
+    }
+    
+    /**
+     * Method for setting current style key.
+     * @param string $sStyleKey style key to be set (key under which style collection is stored)
+     * @param boolean $bIfStyleNotExistsSelectDefault if true and style key was not found, default style key is set 
+     * (if it is like by default false, previously set style key remains)
+     * @return boolean true if style was found, false otherwise
      */
     public function setStyleKey( $sStyleKey, $bIfStyleNotExistsSelectDefault = false )
     {
@@ -80,11 +100,23 @@ class KrscReports_Type_Excel_PHPExcel_Cell
        return $bIsValid;
     }
     
+    /**
+     * Method returns letter coordinate for numeric coordinate of column.
+     * @param integer $iColumnId numeric coordinate of column (starts from 0)
+     * @return string letter coordinate of given column
+     */
     public function getColumnDimension( $iColumnId )
     {
         return self::$_oPHPExcel->getActiveSheet()->getColumnDimensionByColumn( $iColumnId )->getColumnIndex();
     }
     
+    /**
+     * Method creates cell with previously set properties on given in method's input coordinates 
+     * (with one object with set properties more than one cell can be created).
+     * @param integer $iColumnId numeric coordinate of column (starts from 0)
+     * @param integer $iRowId numeric coordinate of row (starts from 1)
+     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
+     */
     public function constructCell( $iColumnId, $iRowId )
     {        
         // setting styles
@@ -92,6 +124,8 @@ class KrscReports_Type_Excel_PHPExcel_Cell
         
         // constructing phpexcel element
         self::$_oPHPExcel->getActiveSheet()->setCellValueByColumnAndRow( $iColumnId, $iRowId, $this->_mValue );        
+        
+        return $this;
     }
     
 }
--- Classes/KrscReports/Type/Excel/PHPExcel/Style.php
+++ Classes/KrscReports/Type/Excel/PHPExcel/Style.php
@@ -1,31 +1,50 @@
 <?php
 /**
-  @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
+ * Class for managing style collections.
+ * 
+ * @category KrscReports
+ * @package KrscReports_Type
+ * @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
  */
 class KrscReports_Type_Excel_PHPExcel_Style
 {
+    /**
+     * default key of style collection
+     */
     const KEY_DEFAULT = 'default';
     
     /**
-     * @var Array array of KrscReports_Type_Excel_PHPExcel_Style_Iterator_Iterator
+     * @var array array of KrscReports_Type_Excel_PHPExcel_Style_Iterator_Iterator
      */
     protected $_aStyleIterator;
     
+    /**
+     * Method setting style collection on given key.
+     * @param KrscReports_Type_Excel_PHPExcel_Style_Iterator_Collection $oStyleCollection style collection to be set
+     * @param string $sKey key under which given style collection will be set
+     * @return KrscReports_Type_Excel_PHPExcel_Style object on which method was executed
+     */
     public function setStyleCollection( KrscReports_Type_Excel_PHPExcel_Style_Iterator_Collection $oStyleCollection, $sKey = self::KEY_DEFAULT )
     {
         $this->_aStyleIterator[$sKey] = new KrscReports_Type_Excel_PHPExcel_Style_Iterator_Iterator( $oStyleCollection );
+        return $this;
     }
     
     /**
-     * 
-     * @param String $sKey key to verify
-     * @return Boolean true if style key exists, false otherwise
+     * Method checking whether given key of style collection exists.
+     * @param string $sKey key of style collection to verify
+     * @return boolean true if style key exists, false otherwise
      */
     public function isValidStyleKey( $sKey )
     {
         return isset( $this->_aStyleIterator[$sKey] ) ? true : false;
     }
     
+    /**
+     * Method returns array with style properties ready to be inserted to PHPExcel.
+     * @param string $sKey key of style collection which is retrieved (if it is not set, default key is used)
+     * @return array style properties ready to be implemented via applyFromArray method on PHPExcel_Style object
+     */
     public function getStyleArray( $sKey = self::KEY_DEFAULT ) 
     {
         if( !isset( $this->_aStyleIterator[$sKey] ) )
