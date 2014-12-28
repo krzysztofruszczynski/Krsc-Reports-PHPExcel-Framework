4b8f5b8ff2e07bd956fc3e54d8f18092629c812a
ruszczak <ruszczak@o2.pl>
2014-12-28 20:59:45 +0100

    lgpl license comment added

--- Classes/KrscReports/Report/ExampleReportManyStyles.php
+++ Classes/KrscReports/Report/ExampleReportManyStyles.php
@@ -1,5 +1,31 @@
 <?php
 /**
+ * This file is part of KrscReports.
+ *
+ * Copyright (c) 2014 Krzysztof Ruszczyński
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @category KrscReports
+ * @package KrscReports_Report
+ * @copyright Copyright (c) 2014 Krzysztof Ruszczyński
+ * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version 1.0.0, 2014-12-28
+ */
+
+/**
  * Example report creating two tables in different worksheets with borders and fillment (difference in style between header and rows) in PHPExcel. 
  * 
  * @category KrscReports
