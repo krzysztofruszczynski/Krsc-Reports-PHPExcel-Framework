24825cf9ebe8f85ad16abb63a5a9c1bdf0f8143a
Krzysztof Ruszczynski <ruszczak@o2.pl>
2017-10-13 10:40:59 +0200

    - adding license text

--- Classes/KrscReports/Type/Excel/PHPExcel/Style/Bundle/AbstractStyle.php
+++ Classes/KrscReports/Type/Excel/PHPExcel/Style/Bundle/AbstractStyle.php
@@ -1,6 +1,39 @@
 <?php
 namespace KrscReports\Type\Excel\PHPExcel\Style\Bundle;
 
+/**
+ * This file is part of KrscReports.
+ *
+ * Copyright (c) 2017 Krzysztof Ruszczyński
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @category KrscReports
+ * @package KrscReports
+ * @copyright Copyright (c) 2017 Krzysztof Ruszczyński
+ * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version 1.1.6, 2017-10-13
+ */
+
+/**
+ * Abstract class for storing information about styles.
+ * 
+ * @category KrscReports
+ * @package KrscReports
+ * @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
+ */
 abstract class AbstractStyle
 {
     /**
--- Classes/KrscReports/Views/SingleTableWithGraphs.php
+++ Classes/KrscReports/Views/SingleTableWithGraphs.php
@@ -2,7 +2,37 @@
 namespace KrscReports\Views;
 
 /**
+ * This file is part of KrscReports.
+ *
+ * Copyright (c) 2017 Krzysztof Ruszczyński
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @category KrscReports
+ * @package KrscReports
+ * @copyright Copyright (c) 2017 Krzysztof Ruszczyński
+ * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version 1.1.6, 2017-10-13
+ */
+
+/**
  * Report consisting of one single table and graphs associated with it (using data from this table as source).
+ * 
+ * @category KrscReports
+ * @package KrscReports
+ * @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
  */
 class SingleTableWithGraphs extends SingleTable
 {
