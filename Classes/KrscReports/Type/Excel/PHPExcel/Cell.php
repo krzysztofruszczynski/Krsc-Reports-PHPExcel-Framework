db792e6eb6d89c8147307b1fa57bc99131f5bccd
Krzysztof Ruszczynski <ruszczak@o2.pl>
2018-09-27 17:00:31 +0200

    - separation of logic for PHPExcel and PhpSpreadsheet regarding cell

--- Classes/KrscReports/Type/Excel/PHPExcel/Cell.php
+++ Classes/KrscReports/Type/Excel/PHPExcel/Cell.php
@@ -1,4 +1,6 @@
 <?php
+use KrscReports\Type\Excel;
+
 /**
  * This file is part of KrscReports.
  *
@@ -22,7 +24,7 @@
  * @package KrscReports_Type
  * @copyright Copyright (c) 2018 Krzysztof Ruszczyński
  * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
- * @version 1.2.8, 2018-09-17
+ * @version 2.0.0, 2018-09-25
  */
 
 /**
@@ -35,43 +37,13 @@
  * @package KrscReports_Type
  * @author Krzysztof Ruszczyński <http://www.ruszczynski.eu>
  */
-class KrscReports_Type_Excel_PHPExcel_Cell
+class KrscReports_Type_Excel_PHPExcel_Cell extends Excel\Cell
 {
     /**
      * @var PHPExcel instance of used PHPExcel object
      */
     protected static $_oPHPExcel;
-    
-    /**
-     * @var mixed value of current cell
-     */
-    protected $_mValue;
-    
-    /**
-     * @var string type of current cell 
-     */
-    protected $_sType;
-    
-    /**
-     * @var array|integer key is columnId, value is column fixed size (if integer - value apply to every column)
-     */
-    protected $_mColumnFixedSizes = array();
-    
-    /**
-     * @var array|integer key is columnId, value is column fixed size  (if integer - value apply to every column)
-     */
-    protected $_mColumnMaxSizes = array();
 
-    /**
-     * @var KrscReports_Type_Excel_PHPExcel_Style object for managing style collections
-     */
-    protected $_oStyle;
-    
-    /**
-     * @var string currently selected key of style collection
-     */
-    protected $_sStyleKey = KrscReports_Type_Excel_PHPExcel_Style::KEY_DEFAULT;
-    
     /**
      * Constructor which updates PHPExcel instance inside a class.
      * @return Void
@@ -83,38 +55,7 @@ class KrscReports_Type_Excel_PHPExcel_Cell
         // creates default object for managing style collection to handle situations, when user does not set style object
         $this->_oStyle = new KrscReports_Type_Excel_PHPExcel_Style();
     }
-    
-    /**
-     * Method returns current style object (useful for adding new styles).
-     * @return KrscReports_Type_Excel_PHPExcel_Style currently set style object
-     */
-    public function getStyleObject()
-    {
-        return $this->_oStyle;
-    }
-    
-    /**
-     * Method for setting object for managing style collections.
-     * @param KrscReports_Type_Excel_PHPExcel_Style $oStyle instance of object for managing style collections
-     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
-     */
-    public function setStyleObject( KrscReports_Type_Excel_PHPExcel_Style $oStyle )
-    {
-        $this->_oStyle = $oStyle;
-        return $this;
-    }
-    
-    /**
-     * Method for setting value of current cell.
-     * @param mixed $mValue value of current cell (can be text, numeric or formula: starts with =)
-     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
-     */
-    public function setValue( $mValue )
-    {
-        $this->_mValue = $mValue;
-        return $this;
-    }
-    
+
     /**
      * Getter for cell value (from loaded file or from PHPExcel object created during script execution)
      *
@@ -171,40 +112,6 @@ class KrscReports_Type_Excel_PHPExcel_Cell
     }
 
     /**
-     * Method setting type of cell.
-     * @param string $sType type of cell to be set
-     * @return KrscReports_Type_Excel_PHPExcel_Cell object on which method was executed
-     */
-    public function setType( $sType )
-    {
-        $this->_sType = $sType;
-        return $this;
-    }
-    
-    /**
-     * Method for setting current style key.
-     * @param string $sStyleKey style key to be set (key under which style collection is stored)
-     * @param boolean $bIfStyleNotExistsSelectDefault if true and style key was not found, default style key is set 
-     * (if it is like by default false, previously set style key remains)
-     * @return boolean true if style was found, false otherwise
-     */
-    public function setStyleKey( $sStyleKey, $bIfStyleNotExistsSelectDefault = false )
-    {
-       $bIsValid = $this->_oStyle->isValidStyleKey( $sStyleKey );
-       
-       if( $bIsValid )
-       {   // style key exists 
-           $this->_sStyleKey = $sStyleKey; 
-       }
-       else if( $bIfStyleNotExistsSelectDefault )
-       {   // style key not exists but user wanted in such situation to switch to default style (otherwise - previously set style is used) 
-           $this->_oStyle = KrscReports_Type_Excel_PHPExcel_Style::KEY_DEFAULT;
-       }
-       
-       return $bIsValid;
-    }
-    
-    /**
      * Method for getting column dimension.
      * @param integer $iColumnId column index (starts from 0)
      * @return PHPExcel_Worksheet_ColumnDimension column dimension for selected in input columnId
@@ -221,7 +128,7 @@ class KrscReports_Type_Excel_PHPExcel_Cell
      */
     public function getColumnDimension( $iColumnId )
     {
-        return $this->_getColumnDimensionByColumn( $iColumnId )->getColumnIndex();        
+        return $this->_getColumnDimensionByColumn( $iColumnId )->getColumnIndex();
     }
 
     /**
@@ -233,97 +140,6 @@ class KrscReports_Type_Excel_PHPExcel_Cell
     public function setColumnDimensionAutosize( $iColumnId, $bAutoSize = true )
     {
         return $this->_getColumnDimensionByColumn( $iColumnId )->setAutoSize( $bAutoSize );
-    }
-
-    /**
-     * Method checks whether fixed size for this column is already set.
-     *
-     * @param integer $iColumnId numeric id of column
-     *
-     * @return boolean true if fixed size is already set, false otherwise
-     */
-    public function isColumnFixedSizeIsSet( $iColumnId )
-    {
-        $bIsColumnFixedSizeIsSet = false;
-
-        if (!is_array($this->_mColumnFixedSizes) && is_numeric($this->_mColumnFixedSizes)) {
-            $bIsColumnFixedSizeIsSet = true;
-        } else {
-            $bIsColumnFixedSizeIsSet = isset($this->_mColumnFixedSizes[$iColumnId]);
-        }
-
-        return $bIsColumnFixedSizeIsSet;
-    }
-
-    /**
-     * Method setting fixed size for specific column.
-     * @param integer|null $iColumnId numeric id of column (null - apply to all columns)
-     * @param double $dFixedSize width of column
-     *
-     * @return void PHPExcel_Worksheet_ColumnDimension|void column dimension for selected in input columnId (if set)
-     */
-    public function setColumnFixedSize($iColumnId, $dFixedSize)
-    {
-        if (is_null($iColumnId)) {
-            $this->_mColumnFixedSizes = $dFixedSize;
-        } else {
-            $this->_mColumnFixedSizes[$iColumnId] = $dFixedSize;
-        }
-    }
-
-    /**
-     * Method checks whether max size for this column is already set.
-     *
-     * @param integer $iColumnId numeric id of column
-     *
-     * @return boolean true if max size is already set, false otherwise
-     */
-    public function isColumnMaxSizeIsSet($iColumnId)
-    {
-        $bIsColumnMaxSizeIsSet = false;
-
-        if (!is_array($this->_mColumnMaxSizes) && is_numeric($this->_mColumnMaxSizes)) {
-            $bIsColumnMaxSizeIsSet = true;
-        } else {
-            $bIsColumnMaxSizeIsSet = isset($this->_mColumnMaxSizes[$iColumnId]);
-        }
-
-        return $bIsColumnMaxSizeIsSet;
-    }
-
-    /**
-     * Method for getting max column size.
-     *
-     * @param integer $iColumnId column Id
-     *
-     * @return integer max size of column
-     */
-    public function getColumnMaxSize($iColumnId)
-    {
-        if (is_array($this->_mColumnMaxSizes)) {
-            $iMaxSize = $this->_mColumnMaxSizes[$iColumnId];
-        } else {
-            $iMaxSize = $this->_mColumnMaxSizes;
-        }
-
-        return $iMaxSize;
-    }
-
-    /**
-     * Method setting max size for specific column.
-     *
-     * @param integer|null $iColumnId numeric id of column (null - apply to all columns)
-     * @param double $dMaxSize max width of column
-     *
-     * @return void
-     */
-    public function setColumnMaxSize($iColumnId, $dMaxSize)
-    {
-        if (is_null($iColumnId)) {
-            $this->_mColumnMaxSizes = $dMaxSize;
-        } else {
-            $this->_mColumnMaxSizes[$iColumnId] = $dMaxSize;
-        }
     }
 
     /**
@@ -430,5 +246,15 @@ class KrscReports_Type_Excel_PHPExcel_Cell
         self::$_oPHPExcel->getActiveSheet()->mergeCellsByColumnAndRow($iBeginColumnId, $iBeginRowId, $iEndColumnId, $iEndRowId);
 
         return $this;
+    }
+
+    /**
+     * Method attaching chart to active sheet.
+     *
+     * @param \PHPExcel_Chart $oChart object with chart
+     */
+    public function attachChart($oChart)
+    {
+        self::$_oPHPExcel->getActiveSheet()->addChart($oChart);
     }
 }
