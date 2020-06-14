9598bcc87758fa8e3169f8aee223455eebb1db59
Krzysztof Ruszczyński <ruszczak@o2.pl>
2020-06-14 19:39:54 +0200

    Update Service.php
    - phpdoc update

--- Classes/KrscReports/Service.php
+++ Classes/KrscReports/Service.php
@@ -4,7 +4,7 @@ namespace KrscReports;
 /**
  * This file is part of KrscReports.
  *
- * Copyright (c) 2017 Krzysztof Ruszczyński
+ * Copyright (c) 2020 Krzysztof Ruszczyński
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,9 +22,9 @@ namespace KrscReports;
  *
  * @category KrscReports
  * @package KrscReports
- * @copyright Copyright (c) 2018 Krzysztof Ruszczyński
+ * @copyright Copyright (c) 2020 Krzysztof Ruszczyński, Simon Heimberg
  * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
- * @version 1.2.4, 2018-03-23
+ * @version 2.1.2, 2020-06-14
  */
 
 /**
